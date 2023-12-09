2a607f209e9c8b23debcbd9489db80ab81f78ab9
Dmitry Subbotin <sybbotin1234@yandex.ru>
2023-12-09 20:13:00 +0300

    update lab9
    upload and update lab9 files (asd)

---
+++ Third Semester/Algorithms and data structures/lab9/lab9(DFS)/lab9/lab9/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace lab9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(394, 389);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(166, 153);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Задать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(8, 29);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(166, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(424, 303);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Нарисовать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.VisualizeGraph);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(424, 213);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(184, 84);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Количество веришин";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(5, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(419, 412);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Визуализация графа";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Location = new System.Drawing.Point(423, 20);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(185, 187);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Матрица смежности";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBox1);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Controls.Add(this.numericUpDown2);
+            this.groupBox4.Controls.Add(this.button3);
+            this.groupBox4.Controls.Add(this.richTextBox1);
+            this.groupBox4.Location = new System.Drawing.Point(626, 26);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(174, 300);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Обход в глубину";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Значение для поиска";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Начальная вершина";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(6, 54);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(163, 20);
+            this.numericUpDown2.TabIndex = 2;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 271);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(162, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Выполнить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(7, 140);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(162, 125);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(726, 401);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Очистить всё";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.ColumnHeadersVisible = false;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 14);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.Size = new System.Drawing.Size(178, 35);
+            this.dataGridView2.TabIndex = 11;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(0, 55);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(177, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Здадать значения";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.dataGridView2);
+            this.groupBox5.Controls.Add(this.button5);
+            this.groupBox5.Location = new System.Drawing.Point(424, 339);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(184, 85);
+            this.groupBox5.TabIndex = 13;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Задать значения узлам";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 426);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Граф по матрице смежноти. Обход в глубину";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.groupBox5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Third Semester/Algorithms and data structures/lab9/lab9(DFS)/lab9/lab9/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using static lab9.Form1;
+
+namespace lab9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+            groupBox4.Enabled = false;
+            groupBox5.Enabled = false;
+
+        }
+        public class GraphNode
+        {
+            public int Index { get; set; }
+            public string Value { get; set; }
+
+            public GraphNode(int index, string value)
+            {
+                Index = index;
+                Value = value;
+            }
+        }
+        int[,] adjacencyMatrix;
+        int graphNodeCount;
+        private List<GraphNode> graphNodes; // Список вершин графа
+        private void button1_Click(object sender, EventArgs e)
+        {
+            graphNodeCount = (int)numericUpDown1.Value;
+            dataGridView1.RowCount = graphNodeCount;
+            dataGridView1.ColumnCount = graphNodeCount;
+            dataGridView2.RowCount = 1;
+            dataGridView2.ColumnCount = graphNodeCount;
+            button2.Enabled = true;
+            graphNodes = new List<GraphNode>();
+            for (int i = 0; i < graphNodeCount; i++)
+            {
+                graphNodes.Add(new GraphNode(i, "Value" + (i + 1)));
+            }
+        }
+
+
+        private void DrawGraph(int[,] adjacencyMatrix)
+        {
+            if (adjacencyMatrix == null) return;
+
+            int vertexCount = adjacencyMatrix.GetLength(0);
+            if (vertexCount == 0) return;
+
+            Bitmap bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+                // Определение координат для вершин, чтобы они равномерно распределялись по окружности
+                PointF[] vertexPositions = CalculateVertexPositions(vertexCount);
+
+                for (int i = 0; i < vertexCount; i++) //отрисовка вершин
+                {
+                    PointF vertexPosition = vertexPositions[i];
+                    g.FillEllipse(Brushes.Blue, vertexPosition.X - 10, vertexPosition.Y - 10, 25, 25);
+                    g.DrawString((i + 1).ToString(), new Font("Arial", 12, FontStyle.Bold), Brushes.White, vertexPosition.X - 5, vertexPosition.Y - 5);
+
+                    //if (i < graphNodes.Count)
+                    //{
+                    //    string nodeValue = graphNodes[i].Value;
+                    //    SizeF textSize = g.MeasureString(nodeValue, new Font("Arial", 12, FontStyle.Bold));
+                    //    PointF textPosition = new PointF(vertexPosition.X - textSize.Width / 2, vertexPosition.Y + 20); // Расположение текста рядом с вершиной
+                    //    g.DrawString(nodeValue, new Font("Arial", 12, FontStyle.Bold), Brushes.Black, textPosition);
+                    //}
+                }
+
+                for (int i = 0; i < vertexCount; i++)  // Отрисовка ребер на основе матрицы смежности
+                {
+                    for (int j = i; j < vertexCount; j++) // Изменение здесь: начните с j = i
+                    {
+                        if (adjacencyMatrix[i, j] == 1) // Проверка на значение в матрице смежности (= 1)
+                        {
+                            if (i == j)
+                            {
+                                g.DrawEllipse(Pens.Black, vertexPositions[i].X - 10, vertexPositions[i].Y - 10, 40, 40); //петля
+                            }
+                            else
+                            {
+                                g.DrawLine(Pens.Black, vertexPositions[i], vertexPositions[j]); //отрисовка ребра между вершинами
+                            }
+                        }
+                    }
+                }
+            }
+            pictureBox1.Image = bitmap;
+        }
+
+        private PointF[] CalculateVertexPositions(int vertexCount)
+        {
+            PointF[] positions = new PointF[vertexCount];
+            float centerX = pictureBox1.Width / 2;
+            float centerY = pictureBox1.Height / 2;
+            float radius = Math.Min(centerX, centerY) - 30;
+
+            for (int i = 0; i < vertexCount; i++)
+            {
+                float angle = (float)(2 * Math.PI * i / vertexCount);
+                float x = centerX + radius * (float)Math.Cos(angle);
+                float y = centerY + radius * (float)Math.Sin(angle);
+                positions[i] = new PointF(x, y);
+            }
+            return positions;
+        }
+        private void VisualizeGraph(object sender, EventArgs e)
+        {
+            int vertexCount = dataGridView1.RowCount;  //количество вершин (размер матрицы)
+            adjacencyMatrix = new int[vertexCount, vertexCount]; // матрица смежности
+
+            for (int i = 0; i < vertexCount; i++)
+            {
+                for (int j = 0; j < vertexCount; j++)
+                {
+                    // Проверка, есть ли значение в ячейке DataGridView
+                    object cellValue = dataGridView1.Rows[i].Cells[j].Value;
+                    if (cellValue != null)
+                    {
+                        adjacencyMatrix[i, j] = Convert.ToInt32(cellValue); // Преобразование и запись в матрицу смежности
+                    }
+                }
+            }
+            DrawGraph(adjacencyMatrix);  //визуализация графа на PictureBox
+            groupBox4.Enabled = true;
+            groupBox5.Enabled = true;
+        }
+        private bool isValueFound = false; // Флаг, указывающий, было ли найдено значение
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            numericUpDown2.Maximum = graphNodeCount;
+            int startVertex = (int)numericUpDown2.Value - 1; //выбранная начальная вершина.
+            string searchValue = textBox1.Text; // Значение, которое нужно найти
+            isValueFound = false; //сброс флага
+            List<int> dfsResult = DFS(adjacencyMatrix, startVertex, searchValue);
+
+            if (isValueFound)
+            {
+                richTextBox1.AppendText("Значение найдено в обходе DFS:\n");
+                foreach (int vertex in dfsResult)
+                {
+                    richTextBox1.AppendText((vertex + 1) + " ");
+                }
+            }
+            else
+            {
+                richTextBox1.AppendText("Значение не найдено в обходе DFS.");
+            }
+        }
+        private List<int> DFS(int[,] adjacencyMatrix, int startVertex, string searchValue)
+        {
+            int vertexCount = adjacencyMatrix.GetLength(0);
+            List<int> dfsResult = new List<int>();
+            bool[] visited = new bool[vertexCount];
+
+            Stack<int> stack = new Stack<int>();
+            stack.Push(startVertex);
+            visited[startVertex] = true;
+
+            while (stack.Count > 0)
+            {
+                int currentVertex = stack.Pop();
+                dfsResult.Add(currentVertex);
+
+                if (graphNodes[currentVertex].Value == searchValue)
+                {
+                    isValueFound = true;
+                    break;
+                }
+
+                for (int i = 0; i < vertexCount; i++)
+                {
+                    if (adjacencyMatrix[currentVertex, i] == 1 && !visited[i])
+                    {
+                        stack.Push(i);
+                        visited[i] = true;
+                    }
+                }
+            }
+            return dfsResult;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            groupBox4.Enabled = false;
+            button2.Enabled = false;
+            dataGridView1.Rows.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //if (graphNodes == null)
+            //{
+            //    MessageBox.Show("Сначала создайте граф и вершины, нажав на кнопку 'Создать граф'.");
+            //    return;
+            //}
+
+            // Очистите список graphNodes перед добавлением новых значений.
+            graphNodes.Clear();
+
+            // Обновите значения узлов из dataGridView2 и сохраните их в graphNodes.
+            for (int i = 0; i < dataGridView2.ColumnCount; i++)
+            {
+                string nodeValue = dataGridView2.Rows[0].Cells[i].Value.ToString();
+                graphNodes.Add(new GraphNode(i, nodeValue));
+            }
+            DrawGraph(adjacencyMatrix);
+        }
+
+    }
+}
