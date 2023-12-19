a33c897a32fb128fb7754aa4733e89afa8b74904
Dmitry Subbotin <sybbotin1234@yandex.ru>
2023-12-19 21:23:23 +0300

    del

--- Third Semester/Task 15/lr15/src/main/java/com/example/lr15/configuration/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.lr15.configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.example.lr15")
-public class AppConfig {
-}
--- Third Semester/Task 15/lr15/src/main/resources/db/migration/V3__aa.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO medicalorganization (name, address, phone, openingtime, closingtime, views)
-VALUES
-    ('Травмпункт', 'Менделеева 16', '88005553535', 7, 21, 0);
