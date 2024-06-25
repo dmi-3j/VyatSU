3f08e3becb58036bc8fee9a4566c154d2616fbea
Субботин Дмитрий <stud159707@vyatsu.ru>
2024-06-25 18:27:23 +0300

    add environment variables

--- Fourth Semester/Practice/awstest/src/main/java/ru/kotik/awstest/configuration/AppConfig.java
+++ Fourth Semester/Practice/awstest/src/main/java/ru/kotik/awstest/configuration/AppConfig.java
@@ -1,9 +1,52 @@
 package ru.kotik.awstest.configuration;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("ru.kotik.awstest")
 public class AppConfig {
+
+    @Value("${aws.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.serviceEndpoint}")
+    private String s3ServiceEndpoint;
+
+    @Value("${aws.bucketName}")
+    private String bucketName;
+
+    @Bean
+    public AmazonS3 s3Client() {
+        AmazonS3 s3Client =  AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKeyId, secretKey)))
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(s3ServiceEndpoint, "ru-msk-1"))
+                .withPathStyleAccessEnabled(true)
+                .build();
+
+        if (!s3Client.doesBucketExistV2(bucketName)) {
+            s3Client.createBucket(new CreateBucketRequest(bucketName));
+            s3Client.setBucketAcl(bucketName, CannedAccessControlList.PublicRead);
+        }
+        return s3Client;
+    }
+
+    @Bean
+    public String bucketName() {
+        return bucketName;
+    }
+
+
 }
--- Fourth Semester/Practice/awstest/src/main/java/ru/kotik/awstest/controllers/S3TestController.java
+++ Fourth Semester/Practice/awstest/src/main/java/ru/kotik/awstest/controllers/S3TestController.java
@@ -1,12 +1,9 @@
 package ru.kotik.awstest.controllers;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.*;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,32 +16,20 @@ import java.io.IOException;
 @Controller
 public class S3TestController {
 
-    private AmazonS3 s3Client;
-    private String bucketName;
+    private final AmazonS3 s3Client;
+    private final String bucketName;
 
-    @PostConstruct
-    private void init() {
-        //BasicAWSCredentials awsCreds = new BasicAWSCredentials("accessKey1", "verySecretKey1");
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials("03307e84ecab4d02ca9ed044", "f21fd124c92cae909ea30a4d505be9e0a6fbee11d3030614eb9ceee2019c80c7");
-        s3Client = AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://127.0.0.1:8000", "us-east-1"))
-                .withPathStyleAccessEnabled(true)
-                .build();
-        bucketName = "testbucket";
-        if (!s3Client.doesBucketExistV2(bucketName)) {
-            s3Client.createBucket(new CreateBucketRequest(bucketName));
-            s3Client.setBucketAcl(bucketName, CannedAccessControlList.PublicRead);
-        }
+    @Autowired
+    public S3TestController(AmazonS3 s3Client, String bucketName) {
+        this.s3Client = s3Client;
+        this.bucketName = bucketName;
     }
-
     @GetMapping("/")
     public String manageS3(Model model) {
         ObjectListing objectListing = s3Client.listObjects(bucketName);
         model.addAttribute("files", objectListing);
         return "testS3";
     }
-
     @PostMapping("/manage/uploadFile")
     public String uploadFile(@RequestParam("file") MultipartFile file) {
         if (!file.isEmpty()) {
@@ -69,7 +54,7 @@ public class S3TestController {
         return "redirect:" + url;
     }
     @GetMapping("/manage/deleteFile/{fileName}")
-    public String disable(@PathVariable String fileName) {
+    public String deleteFile(@PathVariable String fileName) {
         DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(bucketName, fileName);
         s3Client.deleteObject(deleteObjectRequest);
         return "redirect:/";
